991367ccceb68cd40a3b2a95abde2c6716a14afd
Syed-Abdullah-Abrar <syedabdullahabrar5020@gmail.com>
2025-01-15 23:57:53 +0530

    completed first page layout

---
+++ TTT-script.js
@@ -0,0 +1,9 @@
+function showSiteSettings(){
+    let x = document.getElementById("settings");
+    if(x.style.display=='none'){
+        x.style.display=='block';
+    }else{
+        x.style.display=='none';
+    }
+    
+}
