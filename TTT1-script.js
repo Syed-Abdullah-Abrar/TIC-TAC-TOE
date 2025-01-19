bdd5c4179d40f7463b213a48dab66685b3ecf741
Syed-Abdullah-Abrar <syedabdullahabrar5020@gmail.com>
2025-01-19 19:39:57 +0530

    Adding letter selection, entering letter in slots,assigning letter to players,setting maximum score

--- TTT1-script.js
+++ TTT1-script.js
@@ -1,1 +1,50 @@
-prompt("ENTER MAXIMUM SCORE");
+/* Getting maximum score input */
+ let max_score=prompt("ENTER MAXIMUM SCORE");
+ const maximum=document.querySelector("#maxScore_set")
+ const setMaximum = (maxIput) => {
+     maximum.innerText=`MAX SCORE : ${maxIput}`;
+ }
+ setMaximum(max_score)
+
+/* Assigning letters to players*/
+const letter=document.querySelectorAll(".letter_select")
+const p1Letter=document.querySelector("#p1_letter")
+const p2Letter=document.querySelector("#p2_letter")
+
+function assingnment(letter){
+                letter[0].addEventListener("click" , () => {
+                    p1Letter.innerText=letter[0].innerText
+                    p2Letter.innerText=letter[1].innerText
+                })
+                letter[1].addEventListener("click" , () => {
+                    p1Letter.innerText=letter[1].innerText
+                    p2Letter.innerText=letter[0].innerText
+                })
+                
+            }
+
+ assingnment(letter)
+
+/* acessing slots to play game*/
+const slots=document.querySelectorAll(".slot");
+let actPlayer = p1Letter.innerText
+
+slots.forEach((box) => {
+    box.addEventListener("click",()=>{
+        const boxId= box.getAttribute("id");
+        console.log(`Box ${boxId} was clicked`);
+        if(actPlayer===p1Letter.innerText){
+            actPlayer=p2Letter.innerText;
+        }
+        else{
+            actPlayer=p1Letter.innerText
+        }
+        console.log(actPlayer);
+        box.innerText=actPlayer;
+
+        for(let val of boxId){
+            Object.freeze(box)
+        }
+      
+    })
+});
