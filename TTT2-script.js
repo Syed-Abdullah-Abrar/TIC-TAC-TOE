e727246d515d5eab52d0af087fe8e0cfd7b95e15
Syed-Abdullah-Abrar <syedabdullahabrar5020@gmail.com>
2025-01-28 15:39:45 +0530

    Making similar game as player vs player and adding new function to play from computer side

---
+++ TTT2-script.js
@@ -0,0 +1,93 @@
+/* Getting maximum score input */
+let max_score = prompt("ENTER MAXIMUM SCORE NUMBER");
+const setMaximum = (maxInput) => {
+  maximum = document.querySelector("#maxScore_set");
+  if (!isNaN(maxInput) && maxInput != 0) {
+    maximum.innerText = `MAX SCORE : ${maxInput}`;
+  } else {
+    maximum.innerText = `MAX SCORE : ${0}`;
+    alert("ENTER VALID NON ZERO NUMBER...");
+    max_score = Number(prompt("ENTER MAXIMUM SCORE NUMBER"));
+    setMaximum(max_score);
+  }
+};
+setMaximum(max_score);
+/* Assigning letters to players*/
+const letter = document.querySelectorAll(".letter_select");
+const p1Letter = document.querySelector("#p1_letter");
+const p2Letter = document.querySelector("#p2_letter");
+
+function assingnment(letter) {
+  letter[0].addEventListener("click", () => {
+    p1Letter.innerText = letter[0].innerText;
+    p2Letter.innerText = letter[1].innerText;
+  });
+  letter[1].addEventListener("click", () => {
+    p1Letter.innerText = letter[1].innerText;
+    p2Letter.innerText = letter[0].innerText;
+    return ;
+  });
+}
+
+assingnment(letter);
+
+/* acessing slots to play game*/
+const parentBox = document.querySelectorAll(".game_box");
+const slots = document.querySelectorAll(".slot");
+let actPlayer = p1Letter.innerText;
+
+/*lock box */
+function lockBox(id, playingLetter) {}
+
+/*Check win*/
+function winningCheck() {
+  let winningCond = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let combo=[
+    [0 ,1 ,2]
+  ];
+  
+
+//   for (i = 0; i < 8; i++) {
+    if(combo[0]===winningCond[0])
+    console.log("WON")
+  
+}
+
+/* Computer play*/
+function compTurn(actPlayer){
+    let compBox = Math.floor(Math.random()*9);
+    let prefId = slots[compBox];
+    prefId.innerText=actPlayer;
+    return
+}
+
+
+slots.forEach((box) => {
+  box.addEventListener("click", () => {
+    const boxId = box.getAttribute("id");
+    console.log(`Box ${boxId} was clicked`);
+    if (actPlayer === p1Letter.innerText) {
+        actPlayer = p2Letter.innerText;
+    }
+    else{    
+      compTurn(actPlayer);
+      actPlayer = p1Letter.innerText
+    }
+    box.innerText = actPlayer;
+    console.log(actPlayer);
+    console.log(boxId);
+    lockBox(boxId, actPlayer);
+    winningCheck();
+  });
+});
+
